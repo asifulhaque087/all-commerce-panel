952dae8008f6bd116f8a8ca8cbe32e9d0537c4f2
“asifulhaque087” <asifulhaque087@gmail.com>
2023-05-21 00:40:58 +0600

    Add product sidebar menu

--- src/navigation/vertical/index.ts
+++ src/navigation/vertical/index.ts
@@ -4,21 +4,36 @@ import { VerticalNavItemsType } from 'src/@core/layouts/types'
 const navigation = (): VerticalNavItemsType => {
   return [
     {
+      title: 'Products',
+      icon: 'tabler:smart-home',
+      children: [
+        {
+          title: 'Add Product',
+          path: '/product/add'
+        },
+        {
+          title: 'List Products',
+          path: '/product'
+        }
+      ]
+    },
+
+    {
       title: 'Home',
       path: '/home',
-      icon: 'tabler:smart-home',
+      icon: 'tabler:smart-home'
     },
     {
       title: 'Second Page',
       path: '/second-page',
-      icon: 'tabler:mail',
+      icon: 'tabler:mail'
     },
     {
       path: '/acl',
       action: 'read',
       subject: 'acl-page',
       title: 'Access Control',
-      icon: 'tabler:shield',
+      icon: 'tabler:shield'
     }
   ]
 }
